9c9265db3859e8c3fae9218f9dbe89eebb95fa2e
Jessie Saunders <Saunders@Jessies-MacBook-Air.local>
2017-11-06 20:42:41 -1000

    create function isGreaterThan

--- exercises.js
+++ exercises.js
@@ -72,6 +72,14 @@ console.log(login("test1234"));
  * Console.log your result.
 */
 
+function isGreaterThan(first, second){
+	if(first > second){
+		return true;
+	}
+}
+
+console.log(isGreaterThan(3,1));
+
 
 
 /*
